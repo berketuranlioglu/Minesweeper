910262dd5396acb83db8420fd50009c5b0f08047
Berke Turanlioglu <berke.turanlioglu@gmail.com>
2023-06-25 18:28:29 +0300

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Berke Turanlıoğlu on 25.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
